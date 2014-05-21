9a8b56b9f156b71155ce1415427e832407302a75
Marcus Paeschke <marcus@mphasize.de>
2014-05-21 18:12:59 +0200

    Fixed normalizePayload, extractMeta & extractLinks for json-api url shorthand style.
    The current versions of the DS.RESTAdapter and DS.RESTSerializer call normalizePayload with only 1 parameter. I removed `type` parameter to fix this.
    
    I am trying to implement this http://jsonapi.org/format/#document-url-template-shorthands, but I didn't try if this change breaks another of the JSON API formats.
    Unfortunately there are no tests for this project at the moment.

--- dist/json_api_adapter.js
+++ dist/json_api_adapter.js
@@ -1,6 +1,6 @@
 /*! 
  * ember-json-api
- * Built on 2014-04-29
+ * Built on 2014-05-21
  * http://github.com/daliwali/ember-json-api
  * Copyright (c) 2014 Dali Zheng
  */
@@ -47,13 +47,13 @@ DS.JsonApiSerializer = DS.RESTSerializer.extend({
   /**
    * Extract top-level "meta" & "links" before normalizing.
    */
-  normalizePayload: function(type, payload) {
+  normalizePayload: function(payload) {
     if(payload.meta) {
-      this.extractMeta(type, payload.meta);
+      this.extractMeta(payload.meta);
       delete payload.meta;
     }
     if(payload.links) {
-      this.extractLinks(type, payload.links);
+      this.extractLinks(payload.links);
       delete payload.links;
     }
     if(payload.linked) {
@@ -84,14 +84,14 @@ DS.JsonApiSerializer = DS.RESTSerializer.extend({
   /**
    * Override this method to parse the top-level "meta" object per type.
    */
-  extractMeta: function(type, meta) {
+  extractMeta: function(meta) {
     // no op
   },
 
   /**
    * Parse the top-level "links" object.
    */
-  extractLinks: function(type, links) {
+  extractLinks: function(links) {
     var link, key, value, route;
 
     for(link in links) {

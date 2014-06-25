e6e6d68961d9f1a2da33d78560398f3a5bd0024d
Stefan Penner <stefan.penner@gmail.com>
2014-06-25 18:08:11 -0400

    make all ajaxErrors return an error object… Somewhat inspired by The AMS adapter

--- src/json_api_adapter.js
+++ src/json_api_adapter.js
@@ -73,6 +73,66 @@ DS.JsonApiAdapter = DS.RESTAdapter.extend({
     var id = get(record, 'id');
 
     return this.ajax(this.buildURL(type.typeKey, id), "PUT", {data: data});
-  }
+  },
+
+  _tryParseErrorResponse:  function(responseText) {
+    try {
+      return response = Ember.$.parseJSON(responseText)
+    } catch(e) {
+      return "Something went wrong";
+    }
+  },
+  ajaxError: function(jqXHR) {
+    var error = this._super(jqXHR);
+    var response;
+
+    if (jqXHR && typeof jqXHR === 'object') {
+      response = this._tryParseErrorResponse(jqXHR.responseText);
+      errors = {};
+
+      if (response &&
+          typeof response === 'object' &&
+          response.errors !== undefined) {
+
+        Ember.A(Ember.keys(response.errors)).forEach(function(key) {
+          errors[Ember.String.camelize(key)] =response.errors[key];
+        });
+      }
+
+      if (jqXHR.status === 422) {
+        return new DS.InvalidError(errors);
+      } else{
+        return new ServerError(jqXHR.status, response, jqXHR);
+      }
+    } else {
+      return error;
+    }
+  },
+      /**
+        Underscores the JSON root keys when serializing.
+
+        @method serializeIntoHash
+        @param {Object} hash
+        @param {subclass of DS.Model} type
+        @param {DS.Model} record
+        @param {Object} options
+      */
+      serializeIntoHash: function(data, type, record, options) {
+        var root = underscore(decamelize(type.typeKey));
+        data[root] = this.serialize(record, options);
+      },
+
 
 });
+
+function ServerError(status, message, xhr) {
+  this.status = status;
+  this.message = message;
+  this.xhr = xhr;
+
+  this.stack = new Error().stack;
+}
+
+ServerError.prototype = Ember.create(Error.prototype);
+ServerError.constructor = ServerError;
+DS.JsonApiAdapter.ServerError = ServerError;

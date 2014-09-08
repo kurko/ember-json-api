d52fee10be40bb98d738d84975654ac8a34fe671
Alexandre de Oliveira <chavedomundo@gmail.com>
2014-09-08 16:13:12 -0300

    Adds Broccoli and moves current code to ES6
    Instead of Grunt, we're moving towards Broccoli for building and
    testing. It's simpler and is being embraced by Ember itself.
    
    This commit makes all tests pass with Broccoli as server. Plus, code is
    now using ES6 modules.

---
+++ Brocfile.js
@@ -0,0 +1,65 @@
+var pickFiles = require('broccoli-static-compiler')
+var mergeTrees = require('broccoli-merge-trees')
+var env = require('broccoli-env').getEnv();
+var compileES6 = require('broccoli-es6-concatenator');
+var findBowerTrees = require('broccoli-bower');
+
+var sourceTrees = [];
+
+if (env === 'production') {
+
+  // Build file
+  sourceTrees = sourceTrees.concat('vendor')
+  var js = new mergeTrees(sourceTrees, { overwrite: true })
+
+  js = compileES6('src', {
+    loaderFile: '../vendor/no_loader.js',
+    inputFiles: [
+      '**/*.js'
+    ],
+    wrapInEval: false,
+    outputFile: '/ember-json-api.js'
+  });
+
+  sourceTrees = sourceTrees.concat(js);
+
+} else if (env === 'development') {
+
+  var src = pickFiles('src', {
+    srcDir: '/',
+    destDir: '/src'
+  });
+
+  sourceTrees = sourceTrees.concat(src)
+  sourceTrees = sourceTrees.concat(findBowerTrees())
+  sourceTrees = sourceTrees.concat('vendor')
+  var js = new mergeTrees(sourceTrees, { overwrite: true })
+
+  js = compileES6(js, {
+    loaderFile: 'loader.js',
+    inputFiles: [
+      'src/**/*.js'
+    ],
+    legacyFilesToAppend: [
+      'jquery.js',
+      'qunit.js',
+      'handlebars.js',
+      'ember.js',
+      'ember-data.js'
+    ],
+    wrapInEval: true,
+    outputFile: '/assets/app.js'
+  });
+
+  sourceTrees = sourceTrees.concat(js);
+
+  var tests = pickFiles('tests', {
+    srcDir: '/',
+    destDir: '/tests'
+  })
+  sourceTrees.push(tests)
+
+  sourceTrees = sourceTrees.concat(tests);
+
+}
+module.exports = mergeTrees(sourceTrees, { overwrite: true });

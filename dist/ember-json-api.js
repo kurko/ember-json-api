a176f4420c2b0ae67029bf703449a441933e30a9
Steffen Brem <Steffen@steffens-mbp.lan>
2014-10-09 21:16:34 +0200

    [Feature] Fixes issue #43
    Added support for href inside a compound object of a link. This way the
    server can point to another url instead of populating all related id’s.

--- dist/ember-json-api.js
+++ dist/ember-json-api.js
@@ -201,7 +201,13 @@
             json[key] = hash[key];
           } else if (typeof hash[key] === 'object') {
             for (var link in hash[key]) {
-              json[link] = hash[key][link];
+              var linkValue = hash[key][link];
+              if (typeof linkValue === 'object' && linkValue.href) {
+                json.links = json.links || {};
+                json.links[link] = linkValue.href;
+              } else {
+                json[link] = linkValue;
+              }
             }
           }
         }
